8939b8f3c4f68b90b583a9242837b332405b0051
HermesThemes <momoboogeyman2000@gmail.com>
2024-11-26 11:34:02 +0000

    add ws

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ import io from "socket.io-client"
 
 
 const app = e()
-let wsclient =  io.connect("https://wired-courses-3vl7.vercel.app/")
+let wsclient =  io.connect("https://wiredcourses-2.onrender.com/")
 
 
 
