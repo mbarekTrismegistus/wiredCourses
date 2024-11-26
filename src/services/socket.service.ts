57eafe802a42501ff8c3d0ef65889e0c5efb9398
HermesThemes <momoboogeyman2000@gmail.com>
2024-11-26 11:12:41 +0000

    add ws

--- src/services/socket.service.ts
+++ src/services/socket.service.ts
@@ -6,7 +6,7 @@ export class SocketService {
     socket:any;
     constructor() {   }
     setupSocketConnection(id:any) {
-      this.socket = io('https://wired-courses-3vl7.vercel.app', {transports: ['polling', 'websocket']});
+      this.socket = io('/ws', {transports: ['polling', 'websocket']});
       this.socket.emit('join', {id: id()})
 
       this.socket.on('sendbacknotif', (msg:any) => {
